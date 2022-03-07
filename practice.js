2826a88917c518e726baaaf9a1aaed54b5bf6749
userHWSeo <gmldnjseocnd@naver.com>
2022-03-07 23:29:59 +0900

    num 30-33
    0307 num 30 ~ 33

---
+++ practice.js
@@ -0,0 +1,31 @@
+// Num 30
+// const ent = "pineapple is yummy";
+// let userEnt = "apple";
+
+// console.log(ent.indexOf(userEnt));
+
+// Num 31
+// 1)  arr[i]              - i에 어떠한 값이 들어가도 시간 복잡도는 O(1)
+// 2)  arr.push(5)         - push는 마지막 요소만 추가하기 때문에 O(1)
+// 3)  arr.slice()         - slice는 start 부터 end까지 복사하여 새로운 객체를 만들기때문에 길이에 따라 처리 시간이 달리져서 O(n)
+// 4)  arr.pop()           - pop은 마지막 요소만 제거하기 때문에 O(1)
+// 5)  arr.includes(5)     - includes는 배열이 특정 값을 포함하는지 확인하는 함수인데 arr[0]부터 검사를 하기 때문에 오랜 시간 걸린다 따라서 O(n)
+
+// Num 32
+
+// const ent = "안녕하세요. 저는 제주대학교 컴퓨터공학전공 혜림입니다.";
+
+// console.log(ent.split(" ").length);
+
+// Num 33
+
+// const ent = "1 2 3 4 5";
+
+// const arrEnt = ent.split(" ").reverse();
+// let reverseVal = "";
+
+// for (let i = 0; i < arrEnt.length; i++) {
+//   reverseVal = reverseVal + arrEnt[i] + " ";
+// }
+
+// console.log(reverseVal);
