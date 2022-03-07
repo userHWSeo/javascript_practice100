1c0865316bcdc0d4a740ab7785f7ab09c500f5fa
userHWSeo <gmldnjseocnd@naver.com>
2022-03-07 23:49:49 +0900

    0307 update
    Num30 ~ Num 33

--- practice.js
+++ practice.js
@@ -5,11 +5,11 @@
 // console.log(ent.indexOf(userEnt));
 
 // Num 31
-// 1)  arr[i]              - i에 어떠한 값이 들어가도 시간 복잡도는 O(1)
-// 2)  arr.push(5)         - push는 마지막 요소만 추가하기 때문에 O(1)
-// 3)  arr.slice()         - slice는 start 부터 end까지 복사하여 새로운 객체를 만들기때문에 길이에 따라 처리 시간이 달리져서 O(n)
-// 4)  arr.pop()           - pop은 마지막 요소만 제거하기 때문에 O(1)
-// 5)  arr.includes(5)     - includes는 배열이 특정 값을 포함하는지 확인하는 함수인데 arr[0]부터 검사를 하기 때문에 오랜 시간 걸린다 따라서 O(n)
+// 1)  arr[i]
+// 2)  arr.push(5)
+// 3)  arr.slice()
+// 4)  arr.pop()
+// 5)  arr.includes(5)
 
 // Num 32
 
